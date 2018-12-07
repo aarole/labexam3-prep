06026c3d5a120e55bdc9ea50e92ced02425b85b3
Ucar <rucar@boilerad.purdue.edu>
2018-12-06 23:08:42 -0500

    Added project files.

---
+++ LabExam03_Prep/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace LabExam03_Prep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.chkTenure = new System.Windows.Forms.CheckBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lstOut = new System.Windows.Forms.ListBox();
+            this.radAsc = new System.Windows.Forms.RadioButton();
+            this.radDesc = new System.Windows.Forms.RadioButton();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.grbSort = new System.Windows.Forms.GroupBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.grbSort.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(40, 22);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(81, 19);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(81, 66);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtSalary.TabIndex = 2;
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.Location = new System.Drawing.Point(40, 69);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(39, 13);
+            this.lblSalary.TabIndex = 3;
+            this.lblSalary.Text = "Salary:";
+            // 
+            // chkTenure
+            // 
+            this.chkTenure.AutoSize = true;
+            this.chkTenure.Location = new System.Drawing.Point(214, 21);
+            this.chkTenure.Name = "chkTenure";
+            this.chkTenure.Size = new System.Drawing.Size(60, 17);
+            this.chkTenure.TabIndex = 4;
+            this.chkTenure.Text = "Tenure";
+            this.chkTenure.UseVisualStyleBackColor = true;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(214, 64);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 5;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lstOut
+            // 
+            this.lstOut.FormattingEnabled = true;
+            this.lstOut.Location = new System.Drawing.Point(43, 127);
+            this.lstOut.Name = "lstOut";
+            this.lstOut.Size = new System.Drawing.Size(246, 134);
+            this.lstOut.TabIndex = 6;
+            this.lstOut.SelectedIndexChanged += new System.EventHandler(this.lstOut_SelectedIndexChanged);
+            // 
+            // radAsc
+            // 
+            this.radAsc.AutoSize = true;
+            this.radAsc.Location = new System.Drawing.Point(9, 21);
+            this.radAsc.Name = "radAsc";
+            this.radAsc.Size = new System.Drawing.Size(75, 17);
+            this.radAsc.TabIndex = 7;
+            this.radAsc.TabStop = true;
+            this.radAsc.Text = "Ascending";
+            this.radAsc.UseVisualStyleBackColor = true;
+            // 
+            // radDesc
+            // 
+            this.radDesc.AutoSize = true;
+            this.radDesc.Location = new System.Drawing.Point(9, 52);
+            this.radDesc.Name = "radDesc";
+            this.radDesc.Size = new System.Drawing.Size(82, 17);
+            this.radDesc.TabIndex = 8;
+            this.radDesc.TabStop = true;
+            this.radDesc.Text = "Descending";
+            this.radDesc.UseVisualStyleBackColor = true;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(464, 44);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 9;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(330, 127);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 10;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(464, 125);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // grbSort
+            // 
+            this.grbSort.Controls.Add(this.radDesc);
+            this.grbSort.Controls.Add(this.radAsc);
+            this.grbSort.Location = new System.Drawing.Point(330, 14);
+            this.grbSort.Name = "grbSort";
+            this.grbSort.Size = new System.Drawing.Size(108, 85);
+            this.grbSort.TabIndex = 12;
+            this.grbSort.TabStop = false;
+            this.grbSort.Text = "Sort";
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(330, 169);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 13;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(455, 169);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(395, 212);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 288);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.grbSort);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.lstOut);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.chkTenure);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LabExam03 - Prep";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.grbSort.ResumeLayout(false);
+            this.grbSort.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.CheckBox chkTenure;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.ListBox lstOut;
+        private System.Windows.Forms.RadioButton radAsc;
+        private System.Windows.Forms.RadioButton radDesc;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox grbSort;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ LabExam03_Prep/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LabExam03_Prep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int cSize = 100;
+        private string[] mName = new string[cSize];
+        private string[] mSalary = new string[cSize]; //If any calculations, declare appropriately (i.e. int or double)
+        private bool[] mTenure = new bool[cSize];
+        private int mIndex = 0;
+        private string mFileName = Path.Combine(Application.StartupPath, "Data.txt");
+
+        private void DisplayMessage(string msg)
+        {
+            MessageBox.Show(msg, Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+        }
+        private bool ValidateInput()
+        {
+            if(txtName.Text == "")
+            {
+                DisplayMessage("Please enter your last name.");
+                txtName.Focus();
+                return false;
+            }
+            if(txtSalary.Text == "")
+            {
+                DisplayMessage("Please enter your salary.");
+                txtSalary.Focus();
+                return false;
+            }
+            double salary;
+            if(double.TryParse(txtSalary.Text, out salary) == false)
+            {
+                DisplayMessage("Please enter a real number in salary.");
+                txtSalary.Focus();
+                return false;
+            }
+            return true;
+
+        }
+        private void ClearInput()
+        {
+            txtName.Clear();
+            txtSalary.Clear();
+            chkTenure.Checked = false;
+
+            return;
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if(ValidateInput() == false)
+            {
+                return;
+            }
+            string name = txtName.Text;
+            string salary = txtSalary.Text;
+
+            mName[mIndex] = name;
+            mSalary[mIndex] = salary;
+            mTenure[mIndex] = chkTenure.Checked;
+            mIndex++;
+
+            if(mIndex == cSize)
+            {
+                DisplayMessage("The arrays are full.");
+                btnEnter.Enabled = false;
+                return;
+            }
+            ClearInput();
+            txtName.Focus();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            lstOut.Items.Clear();
+            if (mIndex == 0)
+            {
+                DisplayMessage("The arrays are empty.");
+                return;
+            }
+            lstOut.Items.Add("Name".PadRight(15) + "Salary".PadRight(15) + "Tenure");
+            lstOut.Items.Add("=================================================================================");
+            for(int ctr = 0; ctr<mIndex; ctr++)
+            {
+                lstOut.Items.Add(mName[ctr].PadRight(15) + mSalary[ctr].PadRight(15) + mTenure[ctr]);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearInput();
+            lstOut.Items.Clear();
+            radAsc.Checked = false;
+            radDesc.Checked = false;
+            txtSearch.Clear();
+            txtName.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lstOut_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lstOut.SelectedIndex < 2)
+            {
+                return;
+            }
+            int index = lstOut.SelectedIndex - 2;
+            txtName.Text = mName[index];
+            txtSalary.Text = mSalary[index];
+            chkTenure.Checked = mTenure[index];
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            lstOut.Items.Clear();
+            if (radAsc.Checked == false && radDesc.Checked == false)
+            {
+                DisplayMessage("Please select a sort option.");
+                return;
+            }
+
+            string[] tempName = new string[cSize];
+            Array.Copy(mName, tempName, mIndex);
+
+            Array.Sort(mName, mSalary, 0, mIndex);
+            Array.Sort(tempName, mTenure, 0, mIndex);
+
+            if(radDesc.Checked == true)
+            {
+                Array.Reverse(mName, 0, mIndex);
+                Array.Reverse(mSalary, 0, mIndex);
+                Array.Reverse(mTenure, 0, mIndex);
+            }
+
+            lstOut.Items.Add("Name".PadRight(15) + "Salary".PadRight(15) + "Tenure");
+            lstOut.Items.Add("=================================================================================");
+            for (int ctr = 0; ctr < mIndex; ctr++)
+            {
+                lstOut.Items.Add(mName[ctr].PadRight(15) + mSalary[ctr].PadRight(15) + mTenure[ctr]);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lstOut.Items.Clear();
+            if (txtSearch.Text == "")
+            {
+                DisplayMessage("Please enter a search term.");
+                return;
+            }
+
+            string search = txtSearch.Text.ToLower();
+            bool flag = false;
+            int ctr;
+
+            for(ctr = 0; ctr<mIndex; ctr++)
+            {
+                if(search == mName[ctr].ToLower())
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(flag == true)
+            {
+                lstOut.Items.Add("Discovered name: " + mName[ctr]);
+                lstOut.Items.Add("Name".PadRight(15) + "Salary".PadRight(15) + "Tenure");
+                lstOut.Items.Add("=================================================================================");
+                lstOut.Items.Add(mName[ctr].PadRight(15) + mSalary[ctr].PadRight(15) + mTenure[ctr]);
+            }
+            else
+            {
+                DisplayMessage("Search term not found.");
+                return;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StreamWriter SW = null;
+
+            if(MessageBox.Show("Do you want to quit the application?", Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+                txtName.Focus();
+                return;
+            }
+            else
+            {
+                try
+                {
+                    SW = new StreamWriter(mFileName);
+                    for (int ctr = 0; ctr < mIndex; ctr++)
+                    {
+                        SW.WriteLine(mName[ctr] + '\t' + mSalary[ctr] + '\t' + mTenure[ctr]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DisplayMessage(ex.Message);
+                }
+                finally
+                {
+                    if (SW != null)
+                    {
+                        SW.Close();
+                    }
+                }
+            }
+        }
+    }
+}
